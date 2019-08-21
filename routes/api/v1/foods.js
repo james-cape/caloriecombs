5733d8359b2cac7765727782f1f4b65ace753a07
Brian Plantico <bplantico@gmail.com>
2019-08-20 19:03:20 -0600

    Adds ability to POST to  with a food object containing a name and calories and create a food object that's saved in the DB

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -32,4 +32,24 @@ router.get("/:id", function(req, res, next) {
   });
 });
 
+/* POST to create a food */
+router.post("/", (req, res) => {
+  if (req.body.food.name && req.body.food.calories) {
+    Food.create({
+      name: req.body.food.name,
+      calories: req.body.food.calories
+    })
+    .then(food => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(201).send(JSON.stringify(food));
+    })
+    .catch(error => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(500).send({error})
+    });
+  } else {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify({error: "Please provide a food with a name and calories."}));
+  }
+})
 module.exports = router;
