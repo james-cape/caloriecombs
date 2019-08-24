ef3046d01da8ec0696839c21ad58cc0d74a14102
James Cape <james.cape@gmail.com>
2019-08-24 16:25:55 -0600

    Adds id to meals seads
    Co-Authored-By: Brian Plantico 
    <43261385+bplantico@users.noreply.github.com>

--- seeders/20190822215057-meal_seed.js
+++ seeders/20190822215057-meal_seed.js
@@ -5,18 +5,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Meal', [
       {
+        id: 1,
         name: "Breakfast",
         createdAt: new Date(),
         updatedAt: new Date()},
       {
+        id: 2,
         name: "Snack",
         createdAt: new Date(),
         updatedAt: new Date()},
       {
+        id: 3,
         name: "Lunch",
         createdAt: new Date(),
         updatedAt: new Date()},
       {
+        id: 4,
         name: "Dinner",
         createdAt: new Date(),
         updatedAt: new Date()},
