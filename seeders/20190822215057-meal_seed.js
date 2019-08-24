169c15ba5c705b16ae13994787b970889a37332c
James Cape <james.cape@gmail.com>
2019-08-24 15:23:02 -0600

    Adds Heroku configurations
    Co-Authored-By: Brian Plantico 
    <43261385+bplantico@users.noreply.github.com>
    
    Co-Authored-By: Brian Plantico <bplantico@users.noreply.github.com>

--- seeders/20190822215057-meal_seed.js
+++ seeders/20190822215057-meal_seed.js
@@ -3,32 +3,28 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
-    return queryInterface.bulkInsert('Food', [
+    return queryInterface.bulkInsert('Meal', [
     {
       id: 1,
-      name: 'banana',
-      calories: 150,
+      name: 'Breakfast',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       id: 2,
-      name: 'mushroom',
-      calories: 105,
+      name: 'Snack',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       id: 3,
-      name: 'salmon',
-      calories: 175,
+      name: 'Lunch',
       createdAt: new Date(),
       updatedAt: new Date()
     },
     {
       id: 4,
-      name: 'peanut butter',
-      calories: 195,
+      name: 'Dinner',
       createdAt: new Date(),
       updatedAt: new Date()
     }
@@ -36,6 +32,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Food', null, {});
+    return queryInterface.bulkDelete('Meal', null, {});
   }
 };
---
+++ seeders/20190824215057-meal_food_seed.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+
+    return queryInterface.bulkInsert('MealFood', [
+      {
+        foodId: 1,
+        mealId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        foodId: 2,
+        mealId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        foodId: 3,
+        mealId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        foodId: 4,
+        mealId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        foodId: 3,
+        mealId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        foodId: 1,
+        mealId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]),
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('MealFood', null, {});
+  }
+};
