827a7df02c7ed691171bcdc34f0e65fc7967f2ea
James Cape <james.cape@gmail.com>
2019-08-28 16:44:51 -0600

    Adds tests for happy and sad servings path
    Co-Authored-By: Brian Plantico 
    <43261385+bplantico@users.noreply.github.com>

--- spec/api/v1/recipes.spec.js
+++ spec/api/v1/recipes.spec.js
@@ -39,4 +39,43 @@ describe('Edamam Microservice API', () => {
       expect(response.body).toEqual({"error": "Include food in query"});
     })
   })
+
+  test('Test GET /api/v1/servings path', async () => {
+    return request('https://edamamservice.herokuapp.com').get('/api/v1/servings?q=2')
+    .then(response => {
+      expect(response.status).toBe(200);
+      expect(response.body.length).toBe(4);
+      return(response.body[0])
+    })
+    .then(recipe => {
+      expect.objectContaining(
+        {
+          id: expect.any(Number),
+          label: expect.any(String),
+          image: expect.any(String),
+          url: expect.any(String),
+          yield: expect.any(Number),
+          calories: expect.any(Number),
+          totalWeight: expect.any(Number),
+          food: expect.any(String)
+        }
+      )
+    })
+  })
+
+  test('Test bad GET /api/v1/servings path with no q param', async () => {
+    return request('https://edamamservice.herokuapp.com').get('/api/v1/servings')
+    .then(response => {
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({"error": "Include servings in query"});
+    })
+  })
+
+  test('Test bad GET /api/v1/servings path no match for that quantity', async () => {
+    return request('https://edamamservice.herokuapp.com').get('/api/v1/servings?q=2000')
+    .then(response => {
+      expect(response.status).toBe(400);
+      expect(response.body).toEqual({"error": "No matches found for that number"});
+    })
+  })
 })
